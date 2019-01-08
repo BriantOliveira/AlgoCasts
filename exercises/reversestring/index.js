775d787f9bc5f52a04069c3b00feb717797acfb0
Briant Oliveira <briantmoliveira@gmail.com>
2019-01-07 16:53:18 -0800

    Adding a 3rd method

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,14 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+/** METHOD 1 */
 // function reverse(str) {
 //   const arr = str.split('');
 //   arr.reverse();
 //   return arr.join('');
 // }
 
+/** METHOD 2 */
 function reverse(str) {
   return str.split('').reverse().join('');
 }
+
+/** METHOD 3 */
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
 
 module.exports = reverse;
