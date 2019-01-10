94c9894a22599a69f724d5b5e9fa7992a02a5029
Briant Oliveira <briantmoliveira@gmail.com>
2019-01-10 11:22:44 -0800

    Adding maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Common String Questions 
+// What is the most common character in the string?
+// Does string A have the same characters as string B (is it an anagram)?
+// Does the given string have any repeated characters in it?
+
+function maxChar(str) {
+  const charsMap = {}
+
+  for (let char of str) {
+    if (charsMap[char]) {
+      charsMap[char]++;
+    } else {
+      charsMap[char] = 1;
+    }
+  }
+
+  /** Method 2  */
+  // for (let char of str) {
+  //   let test = chars[char] = chars[char] + 1 || 1;
+  //   return test
+  // }
+}
+
 
 module.exports = maxChar;
